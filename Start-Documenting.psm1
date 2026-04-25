cce182674b63db36fa4df1bbdda3bd2b6513d736
Mark Gabryjelski <40899144+MarkGabbs@users.noreply.github.com>
2026-04-25 11:23:55 -0400

    Update Start-Documenting.psm1

--- Start-Documenting.psm1
+++ Start-Documenting.psm1
@@ -5,12 +5,12 @@ A simple module, to run "start-transcript" with a consistent file name Has quali
 and use your home directories 'Documents' folder to keep PowerShell transcripts.
 
 Place the Start-Docuemnting.psm1 file into:
-~/Doccuments/PowerShell/Modules/Start-Dcoumenting/Start-Documenting.psm1
+~/Doccuments/PowerShell/Modules/Start-Documentation/Start-Documentation.psm1
 or
-C:\Users%USERNAME%\Doccuments\PowerShell\Modules\Start-Dcoumenting\Start-Documenting.psm1
+C:\Users%USERNAME%\Doccuments\PowerShell\Modules\Start-Documentation\Start-Documentation.psm1
 #>
 
-Function Start-Documenting {
+Function Start-Documentation {
     [CmdletBinding()]
     param(
         [Parameter()]
