aaf2abe606b99bf27c06cae85dcbd6c92cc20ae6
Mark Gabryjelski <40899144+MarkGabbs@users.noreply.github.com>
2026-05-06 10:08:08 -0400

    Update Start-Documentation.psm1

--- Start-Documentation.psm1
+++ Start-Documentation.psm1
@@ -4,7 +4,7 @@ Latest version 2024-09-21
 A simple module, to run "start-transcript" with a consistent file name Has qualifier to check which OS,
 and use your home directories 'Documents' folder to keep PowerShell transcripts.
 
-Place the Start-Docuemnting.psm1 file into:
+Place the Start-Docuemntation.psm1 file into:
 ~/Doccuments/PowerShell/Modules/Start-Documentation/Start-Documentation.psm1
 or
 C:\Users%USERNAME%\Doccuments\PowerShell\Modules\Start-Documentation\Start-Documentation.psm1
