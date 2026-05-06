91e8ab4979a9d773241c6881cedda24bee3b798b
Mark Gabryjelski <40899144+MarkGabbs@users.noreply.github.com>
2026-05-06 10:08:41 -0400

    Update Start-Documentation.psm1

--- Start-Documentation.psm1
+++ Start-Documentation.psm1
@@ -7,7 +7,7 @@ and use your home directories 'Documents' folder to keep PowerShell transcripts.
 Place the Start-Docuemntation.psm1 file into:
 ~/Doccuments/PowerShell/Modules/Start-Documentation/Start-Documentation.psm1
 or
-C:\Users%USERNAME%\Doccuments\PowerShell\Modules\Start-Documentation\Start-Documentation.psm1
+C:\Users\%USERNAME%\Doccuments\PowerShell\Modules\Start-Documentation\Start-Documentation.psm1
 #>
 
 Function Start-Documentation {
